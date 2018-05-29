42ce18b2ec75f276a67adca634d17d2d2983af51
Cleiton Loiola <klaygomes@gmail.com>
2018-05-28 22:25:56 -0300

    chore: include basic nextjs configuration

---
+++ next.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    pageExtensions: ['jsx', 'js'],
+    serverRuntimeConfig: { // Will only be available on the server side
+        mySecret: 'secret'
+      },
+      publicRuntimeConfig: { // Will be available on both server and client
+        staticFolder: '/static'
+      },
+    webpack: (config, { buildId, dev, isServer, defaultLoaders }) => {
+        // Perform customizations to webpack config
+
+        // Important: return the modified config
+        return config
+    }
+}
